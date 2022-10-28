e739681b64f452bfeb649677b4a534ce0935158d
sathishkannan162 <sathishkannan162@gmail.com>
2022-10-28 22:45:24 +0530

    feat: get number from string
    fix: (feat/fix) get number from string
    
    feat: throw error for double decimals and double fractions
    
    feat: return 1 for empty number

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,9 +1,29 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
+    let result = input.match(/[\d\./]+/);
+    if (result == null) {
+      return 1;
+    }
+    // if result is not null, the 0 index contains the number
+    result = result[0];
+    if (/^\d*\.?\d*$/.test(result)) {
+      return eval(result);
+    }
     
-    return result;
+    let operator = result.match(/[/]/g);
+    try {
+      result=eval(result);
+    } catch (error) {
+      throw 'invalid number'
+    }
+    if (operator == null) {
+      return result;
+    }
+    if (operator.length==1) {
+      return result;
+    }
+    throw 'invalid number'
   };
   
   this.getUnit = function(input) {
