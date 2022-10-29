f4744527a84c3783206b706af346b892fcb89222
sathishkannan162 <sathishkannan162@gmail.com>
2022-10-29 16:54:24 +0530

    feat: api for get requests with query

--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,16 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route('/api/convert').get((req,res)=>{
+    let initNum = convertHandler.getNum(req.query.input);
+    let initUnit = convertHandler.getUnit(req.query.input);
+    res.json({
+      initNum,
+      initUnit,
+      returnNum: Number(convertHandler.convert(initNum,initUnit)),
+      returnUnit: convertHandler.getReturnUnit(initUnit),
+      string: convertHandler.getString(initNum,initUnit,convertHandler.convert(initNum,initUnit),convertHandler.getReturnUnit(initUnit))
+    });
+  })
+
 };
