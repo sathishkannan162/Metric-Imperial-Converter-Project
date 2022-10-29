99156b7bf53282df5fa9a92706ab5bb228e9ea8c
sathishkannan162 <sathishkannan162@gmail.com>
2022-10-29 12:03:31 +0530

    refactor: getNum function

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,32 +1,23 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result = input.match(/[\d\./]+/);
-    if (result == null) {
+    let unitRegex = /[a-zA-Z]+$/;
+    // let result = input.match(/[\d\./]+/);
+    let result = input.replace(unitRegex,'');
+    if (result =='') {
       return 1;
     }
-    // if result is not null, the 0 index contains the number
-    result = result[0];
     if (/^\d*\.?\d*$/.test(result)) {
       return eval(result);
     }
-    
-    let operator = result.match(/[/]/g);
-    try {
-      result=eval(result);
-    } catch (error) {
-      throw 'invalid number'
-    }
-    if (operator == null) {
-      return result;
-    }
-    if (operator.length==1) {
-      return result;
+    if (/^\d*\.?\d*\/\d*\.?\d*$/.test(result)) {
+      return eval(result);
     }
     throw 'invalid number'
   };
   
   this.getUnit = function(input) {
+
     let result;
     
     return result;
