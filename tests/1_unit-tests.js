bb7965b79c1a29f6e13c6be69212c4eaba4ca83b
sathishkannan162 <sathishkannan162@gmail.com>
2022-10-29 11:27:58 +0530

    test: (fix/test) test string with units for double fractions

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -26,15 +26,15 @@ suite('Unit Tests', function(){
 
         });
         test('Throw error for double fractions',function(){
-            assert.Throw(()=>{convertHandler.getNum('3/1/2')},'invalid number','Error not thrown for whole number double fractions');
-            assert.Throw(()=>{convertHandler.getNum('3.1/1.1/2')},'invalid number','Error not thrown for mix of decimal doublefractions');
-            assert.Throw(()=>{convertHandler.getNum('3/1/2.1')},'invalid number','Error not thrown for more mix of decimal double fractions');
-            assert.Throw(()=>{convertHandler.getNum('3.7/1.7/0.2')},'invalid number','Error not thrown for three decimal double fractions');
-            assert.Throw(()=>{convertHandler.getNum('3/1/2.1.1')},'invalid number','Error not thrown for double decimal double fractions');
+            assert.Throw(()=>{convertHandler.getNum('3/1/2km')},'invalid number','Error not thrown for whole number double fractions');
+            assert.Throw(()=>{convertHandler.getNum('3.1/1.1/2mi')},'invalid number','Error not thrown for mix of decimal doublefractions');
+            assert.Throw(()=>{convertHandler.getNum('3/1/2.1l')},'invalid number','Error not thrown for more mix of decimal double fractions');
+            assert.Throw(()=>{convertHandler.getNum('3.7/1.7/0.2L')},'invalid number','Error not thrown for three decimal double fractions');
+            assert.Throw(()=>{convertHandler.getNum('3/1/2.1.1lbs')},'invalid number','Error not thrown for double decimal double fractions');
         });
         test('Throw error for double decimals',function(){
-            assert.throw(()=>{convertHandler.getNum('3.1.1')},'invalid number','Error not thrown for double decimals');
-            assert.throw(()=>{convertHandler.getNum('5.7.7/2')},'invalid number','Error not thrown for double decimals with fraction');
+            assert.throw(()=>{convertHandler.getNum('3.1.1Lbs')},'invalid number','Error not thrown for double decimals');
+            assert.throw(()=>{convertHandler.getNum('5.7.7/2gal')},'invalid number','Error not thrown for double decimals with fraction');
         })
 
     })
