e6314d9470b38af31bad95804b93e039ba7cc869
sathishkannan162 <sathishkannan162@gmail.com>
2022-10-29 17:52:59 +0530

    feat: handle invalid unit and numbers in GET

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,46 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
-  app.route('/api/convert').get((req,res)=>{
-    let initNum = convertHandler.getNum(req.query.input);
-    let initUnit = convertHandler.getUnit(req.query.input);
-    res.json({
-      initNum,
-      initUnit,
-      returnNum: Number(convertHandler.convert(initNum,initUnit)),
-      returnUnit: convertHandler.getReturnUnit(initUnit),
-      string: convertHandler.getString(initNum,initUnit,convertHandler.convert(initNum,initUnit),convertHandler.getReturnUnit(initUnit))
-    });
-  })
+  app.route("/api/convert").get((req, res) => {
+    let error1, error2;
+    try {
+      convertHandler.getNum(req.query.input);
+    } catch (error) {
+      error1 = error;
+    }
 
+    try {
+      convertHandler.getUnit(req.query.input);
+    } catch (error) {
+      error2 = error;
+    }
+    if (error1 == "invalid number" && error2 == "invalid unit") {
+      res.send("invalid number and unit");
+    } else if (error1 == "invalid number") {
+      res.send("invalid number");
+    } else if (error2 == "invalid unit") {
+      res.send("invalid unit");
+    } else {
+      let initNum = convertHandler.getNum(req.query.input);
+      let initUnit = convertHandler.getUnit(req.query.input);
+
+      res.json({
+        initNum,
+        initUnit,
+        returnNum: Number(convertHandler.convert(initNum, initUnit)),
+        returnUnit: convertHandler.getReturnUnit(initUnit),
+        string: convertHandler.getString(
+          initNum,
+          initUnit,
+          convertHandler.convert(initNum, initUnit),
+          convertHandler.getReturnUnit(initUnit)
+        ),
+      });
+    }
+  });
 };
